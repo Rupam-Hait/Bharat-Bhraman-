c564b92892398c29dd26cf7c0047bbdbd555a889
Rupam Hait <haitrupam38@gmail.com>
2025-11-18 20:50:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
 
+let todos = [
+  { id: 1, task: "Learn Express", done: false }
+];
+
+app.get('/api/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/api/todos', (req, res) => {
+  const newTodo = { id: todos.length + 1, ...req.body };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});   
